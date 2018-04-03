c854dab205b20eae76267c61ad7bc4d0b5099842
Adam Grare <agrare@redhat.com>
2018-04-03 14:50:57 -0400

    Add power_state to Ovirt::Vm

--- app/models/providers/ovirt/vm.rb
+++ app/models/providers/ovirt/vm.rb
@@ -1,4 +1,13 @@
 module Providers
   class Ovirt::Vm < Infra::Vm
+    POWER_STATES = {
+      'up'        => 'on',
+      'down'      => 'off',
+      'suspended' => 'suspended',
+    }.freeze
+
+    def self.calculate_power_state(raw_power_state)
+      POWER_STATES[raw_power_state] || super
+    end
   end
 end
